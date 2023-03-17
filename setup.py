aa2cb6d4c7094db2fb35651904400f08918f4dba
Zulfi <zulfikharali650@gmail.com>
2023-03-17 07:19:14 -0700

    Info updates

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def get_requirements(file_path:str)->List[str]:
 setup(
 name='mlproject',
 version='0.0.1',
-author='Zulfi',
+author='Zulfi Ali',
 author_email='zulfikharali650@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
