1276d2aca864f3d95d4d7ca15f5ddca309a6e310
Zulfi <zulfikharali650@gmail.com>
2023-03-18 13:47:52 -0700

    Model Trainer with HyperParam tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -38,16 +38,16 @@ class ModelTrainer:
                 )
             
             models={
-                "Random Forest": RandomForestRegressor(),
-                "Decision Tree": DecisionTreeRegressor(),
-                "Gradient Boosting": GradientBoostingRegressor(),
-                "Linear Regression": LinearRegression(),
-                "K-Neighbors Regressor": KNeighborsRegressor(),
-                "XGBRegressor": XGBRegressor(),
-                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
-                "AdaBoost Regressor": AdaBoostRegressor(),
+                "Random Forest":RandomForestRegressor(),
+                "Decision Tree":DecisionTreeRegressor(),
+                "Gradient Boosting":GradientBoostingRegressor(),
+                "Linear Regression":LinearRegression(),
+                "K-Neighbour Regressor":KNeighborsRegressor(),
+                "XGBRegressor":XGBRegressor(),
+                "CatBoosting Regressor":CatBoostRegressor(verbose=False),
+                "AdaBoost Regressor":AdaBoostRegressor()
             }
-
+         
             params={
                 "Decision Tree": {
                     'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
@@ -63,20 +63,24 @@ class ModelTrainer:
                 "Gradient Boosting":{
                     # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
                     'learning_rate':[.1,.01,.05,.001],
-                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
                     # 'criterion':['squared_error', 'friedman_mse'],
                     # 'max_features':['auto','sqrt','log2'],
                     'n_estimators': [8,16,32,64,128,256]
                 },
                 "Linear Regression":{},
-                "K-Neighbors Regressor":{},
+                "K-Neighbour Regressor":{
+                    'n_neighbors':[5,7,9,11],
+                    # 'weights':['uniform','distance'],
+                    # 'algorithm':['ball_tree','kd_tree','brute']
+                },
                 "XGBRegressor":{
                     'learning_rate':[.1,.01,.05,.001],
                     'n_estimators': [8,16,32,64,128,256]
                 },
                 "CatBoosting Regressor":{
                     'depth': [6,8,10],
-                    'learning_rate': [0.01, 0.05, 0.1],
+                    # 'learning_rate': [0.01, 0.05, 0.1],
                     'iterations': [30, 50, 100]
                 },
                 "AdaBoost Regressor":{
