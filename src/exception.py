a3ec9ac254101767f214807d547eadfdb17226b5
Zulfi <zulfikharali650@gmail.com>
2023-03-18 06:54:17 -0700

    Testing Exception and Logging

--- src/exception.py
+++ src/exception.py
@@ -16,4 +16,11 @@ class CustomException(Exception):
 
     def __str__(self) -> str:
         return self.error_message
-    
+
+
+if __name__=="__main__":
+    try:
+        a = 1/0
+    except Exception as e:
+        logging.info("Zero Division Error")
+        raise CustomException(e,sys)
